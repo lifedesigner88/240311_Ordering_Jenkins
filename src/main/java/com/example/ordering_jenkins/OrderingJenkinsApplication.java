b4505cbc54860fc69994b48924f377e68785ae47
SejongPark <lifedesigner88@gmail.com>
2024-03-11 10:50:17 +0900

    first commit

---
+++ src/main/java/com/example/ordering_jenkins/OrderingJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordering_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderingJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/ordering_jenkins/testController.java
@@ -0,0 +1,15 @@
+package com.example.ordering_jenkins;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello spring";
+    }
+
+}
